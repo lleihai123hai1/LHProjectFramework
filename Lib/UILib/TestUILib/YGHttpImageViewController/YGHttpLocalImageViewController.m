b808790817446bcc5fecea42be6ec3cf35cdb8eb
lleihai123@163.com <lleihai123>
2019-04-16 15:15:37 +0800

    的

--- Lib/UILib/TestUILib/YGHttpImageViewController/YGHttpLocalImageViewController.m
+++ Lib/UILib/TestUILib/YGHttpImageViewController/YGHttpLocalImageViewController.m
@@ -10,6 +10,8 @@
 #import "YYImage.h"
 #import "UIImageView+WebCache.h"
 #import "UIImage+WebP.h"
+#import "UIImage+MultiFormat.h"
+#import "SDImageCache.h"
 @interface YGHttpLocalImageViewController (){
 }
 @property(nonatomic,assign)CGFloat ygStartTime;
@@ -21,54 +23,90 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.view.backgroundColor = [UIColor whiteColor];
-    [self addHttpImage];
-    // Do any additional setup after loading the view.
+//    [self addHttpOldImage];
+//    [self addHttpTailorJpegImage];
+    [self addHttpTailorWebpImage];
 }
 
--(void)addHttpImage{
-   
+-(void)addHttpOldImage{
     NSInteger imageViewCount = 10;
     CGFloat width = self.view.width/2;
     CGFloat height = 2*self.view.height/imageViewCount;
-    
     NSMutableArray*url = [NSMutableArray new];
     for (NSInteger i = 1; i <= imageViewCount; i++) {
-         [url addObject: [NSString stringWithFormat:@"old%ld",(long)i]];
-//         [url addObject: [NSString stringWithFormat:@"a%ld",(long)i]];
-//        [url addObject: [NSString stringWithFormat:@"a%ld",(long)i]];
+        [url addObject: [NSString stringWithFormat:@"old%ld",(long)i]];
     }
     self.ygStartTime = CFAbsoluteTimeGetCurrent();
-    NSInteger imageAllCountLength = 0;
     for (NSInteger i = 0 ; i < url.count; i++) {
         UIImageView* uiImageView = [[UIImageView alloc]initWithFrame:CGRectMake((i%2)*width, i/2*height, width, height)];
         uiImageView.contentMode = UIViewContentModeScaleAspectFit;
         uiImageView.layer.borderWidth = 2;
         uiImageView.layer.borderColor = [UIColor blackColor].CGColor;
-        
-//        NSString *SuperBgImgPath = [[NSBundle mainBundle] pathForResource:url[i] ofType:@"webp"];
         NSString *SuperBgImgPath = [[NSBundle mainBundle] pathForResource:url[i] ofType:@"jpeg"];
         NSData *data  = [NSData dataWithContentsOfFile: SuperBgImgPath];
-        uiImageView.image = [UIImage sd_imageWithWebPData:data];
-//        uiImageView.image = [YYImage imageNamed:url[i]];
+        uiImageView.image = [UIImage sd_imageWithData:data];
         [self.view addSubview:uiImageView];
-        imageAllCountLength += UIImageJPEGRepresentation(uiImageView.image, 1).length;
     }
-    
-    
-    NSLog(@"imageAllCountLength: %ld", (long)imageAllCountLength);
     self.ygEndTime = CFAbsoluteTimeGetCurrent();
+    [[SDImageCache sharedImageCache]clearMemory];
+    [[SDImageCache sharedImageCache]clearDiskOnCompletion:^{
+    }];
+    NSLog(@"原图，客户端的载入时间: %f", self.ygEndTime - self.ygStartTime);
+    
 }
--(void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
-    NSLog(@"YG: Download All Time: %f", self.ygEndTime - self.ygStartTime);
+
+-(void)addHttpTailorJpegImage{
+    NSInteger imageViewCount = 10;
+    CGFloat width = self.view.width/2;
+    CGFloat height = 2*self.view.height/imageViewCount;
+    NSMutableArray*url = [NSMutableArray new];
+    for (NSInteger i = 1; i <= imageViewCount; i++) {
+         [url addObject: [NSString stringWithFormat:@"a%ld",(long)i]];
+    }
+    self.ygStartTime = CFAbsoluteTimeGetCurrent();
+    for (NSInteger i = 0 ; i < url.count; i++) {
+        UIImageView* uiImageView = [[UIImageView alloc]initWithFrame:CGRectMake((i%2)*width, i/2*height, width, height)];
+        uiImageView.contentMode = UIViewContentModeScaleAspectFit;
+        uiImageView.layer.borderWidth = 2;
+        uiImageView.layer.borderColor = [UIColor blackColor].CGColor;
+        NSString *SuperBgImgPath = [[NSBundle mainBundle] pathForResource:url[i] ofType:@"jpeg"];
+        NSData *data  = [NSData dataWithContentsOfFile: SuperBgImgPath];
+        uiImageView.image = [UIImage sd_imageWithData:data];
+        [self.view addSubview:uiImageView];
+    }
+    self.ygEndTime = CFAbsoluteTimeGetCurrent();
+    [[SDImageCache sharedImageCache]clearMemory];
+    [[SDImageCache sharedImageCache]clearDiskOnCompletion:^{
+    }];
+    NSLog(@"裁剪后，以jpeg返回，客户端的载入时间: %f", self.ygEndTime - self.ygStartTime);
+    
 }
-/*
- #pragma mark - Navigation
- 
- // In a storyboard-based application, you will often want to do a little preparation before navigation
- - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
- // Get the new view controller using [segue destinationViewController].
- // Pass the selected object to the new view controller.
- }
- */
+
+
+-(void)addHttpTailorWebpImage{
+    NSInteger imageViewCount = 10;
+    CGFloat width = self.view.width/2;
+    CGFloat height = 2*self.view.height/imageViewCount;
+    NSMutableArray*url = [NSMutableArray new];
+    for (NSInteger i = 1; i <= imageViewCount; i++) {
+        [url addObject: [NSString stringWithFormat:@"a%ld",(long)i]];
+    }
+    self.ygStartTime = CFAbsoluteTimeGetCurrent();
+    for (NSInteger i = 0 ; i < url.count; i++) {
+        UIImageView* uiImageView = [[UIImageView alloc]initWithFrame:CGRectMake((i%2)*width, i/2*height, width, height)];
+        uiImageView.contentMode = UIViewContentModeScaleAspectFit;
+        uiImageView.layer.borderWidth = 2;
+        uiImageView.layer.borderColor = [UIColor blackColor].CGColor;
+        NSString *SuperBgImgPath = [[NSBundle mainBundle] pathForResource:url[i] ofType:@"webp"];
+        NSData *data  = [NSData dataWithContentsOfFile: SuperBgImgPath];
+        uiImageView.image = [UIImage sd_imageWithWebPData:data];
+        [self.view addSubview:uiImageView];
+    }
+    self.ygEndTime = CFAbsoluteTimeGetCurrent();
+    [[SDImageCache sharedImageCache]clearMemory];
+    [[SDImageCache sharedImageCache]clearDiskOnCompletion:^{
+    }];
+    NSLog(@"裁剪后，以webp返回，客户端的载入时间: %f", self.ygEndTime - self.ygStartTime);
+}
 
 @end
